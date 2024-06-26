14b3fdd09299e0ff1457b869a669c321d70175a3
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-06-26 17:45:47 +0200

    Update services.py

--- services.py
+++ services.py
@@ -80,10 +80,17 @@ def find_most_similar_labels(user_input, embeddings, labels, tgt_lang, top_k=5):
 
 def load_embeddings_and_labels():
     try:
+        logger.info(f"Attempting to load embeddings from {combined_embeddings_file}")
         df_embeddings = pd.read_csv(combined_embeddings_file)
-        embeddings = df_embeddings.values
+        logger.info(f"Successfully loaded embeddings. Shape: {df_embeddings.shape}")
+
+        logger.info(f"Attempting to load labels from {excel_file_path}")
         df_labels = pd.read_excel(excel_file_path, engine='odf')
+        logger.info(f"Successfully loaded labels. Shape: {df_labels.shape}")
+
+        embeddings = df_embeddings.values
         labels = df_labels['preferredLabel'].values
         return embeddings, labels
     except Exception as e:
+        logger.error(f"Error loading embeddings or Excel file: {str(e)}")
         raise RuntimeError(f"Error loading embeddings or Excel file: {str(e)}")
