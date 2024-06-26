3cedc216db8a4bdc9accfeff1f971bfb9e460953
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-06-26 13:41:55 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
-from controllers import predict_route as predict, download_data
-from models import TextRequest
+# Dans routes.py
+from fastapi import FastAPI, HTTPException
+from controllers import predict, download_data  # Importer directement les fonctions nécessaires
+from models import TextRequest  # Importer la classe TextRequest depuis models.py
 
 app = FastAPI()
 
 @app.post("/predict/")
 def predict_route(request: TextRequest):
-    return predict(request)
+    return predict(request)  # Utiliser la fonction predict définie dans controllers.py
 
 @app.get("/download-data/")
 def download_data_route():
-    return download_data()
+    return download_data()  # Utiliser la fonction download_data définie dans controllers.py
