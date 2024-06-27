7004923348662b5f0e381629e7c224cc40ad92e5
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-06-27 13:53:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,28 +22,23 @@ tgt_lang = st.selectbox("Langue cible :", options=list(language_options.keys()),
 input_text = st.text_area("Entrez le texte :", "")
 
 # Bouton pour déclencher la traduction et la recherche de similarité
-# Button to trigger translation and similarity search
 if st.button("Traduire et Trouver des Similitudes"):
     if input_text:
         try:
-            # Sending POST request to FastAPI with selected language options
             response = requests.post(
                 "http://localhost:8000/predict/",
                 json={"text": input_text, "src_lang": src_lang, "tgt_lang": tgt_lang},
                 headers={"Content-Type": "application/json"}
             )
-            response.raise_for_status()  # Handling HTTP errors
+            response.raise_for_status()
             
-            # Decode the response content
             result = json.loads(response.content.decode('utf-8'))
             
-            # Displaying original and translated text
             if "user_text" in result:
                 st.write(f"Texte original ({language_options[src_lang]}) :", result["user_text"])
             if "translated_text" in result:
                 st.write(f"Texte traduit ({language_options[tgt_lang]}) :", result["translated_text"])
             
-            # Displaying similarity results
             if "results" in result and isinstance(result["results"], list):
                 st.subheader(f"Similitudes ({language_options[tgt_lang]}) :")
                 for idx, res in enumerate(result["results"], 1):
