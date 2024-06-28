44c608ff75df7cae28ab8f1ea603a28c6cac7113
YAHYA <155691167+zakariaeyahya@users.noreply.github.com>
2024-06-28 11:49:53 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from controllers import predict_controller, download_data_controller, clear_history_controller, last_prediction_controller, stats_controller, supported_languages_controller
-from models import TextRequest
 
 app = FastAPI()
 
-@app.post("/predict/")
-def predict_route(request: TextRequest):
-    return predict_controller(request)
+@app.get("/predict/")
+def predict_route(
+    text: str = Query(..., description="Texte à traduire et à analyser"),
+    src_lang: str = Query(..., description="Code de la langue source"),
+    tgt_lang: str = Query(..., description="Code de la langue cible")
+):
+    return predict_controller(text, src_lang, tgt_lang)
 
 @app.get("/download-data/")
 def download_data_route():
     return download_data_controller()
 
-@app.post("/clear-history/")
+@app.get("/clear-history/")
 def clear_history_route():
     return clear_history_controller()
 
